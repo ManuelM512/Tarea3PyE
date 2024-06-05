72122b9ae09de9226090e2d2eaf46c512aebc5d3
Passeriade <manuel5mongelos@gmail.com>
2024-06-05 12:25:24 -0300

    Deleted unnecesary files

--- prueba.py
+++
@@ -1,9 +0,0 @@
-from distributionSamples import binomSamples
-from exercise import makeExercise
-
-n = 100
-p = 0.35
-
-labels = ["100 samples", "1000 samples", "10000 samples", "100000 samples"]
-binomDistributions = binomSamples(n, p, [100, 1000, 10000, 100000])
-makeExercise("1", "Binom distribution", binomDistributions, labels)
